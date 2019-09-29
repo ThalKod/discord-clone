936fc1f1002e203c2b8a4f5e2405b6ddfa251750
Great Plains Modding <46289624+greatplainsmodding@users.noreply.github.com>
2019-09-28 20:52:59 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {};
 
 config.port = process.env.PORT || 5000;
-config.dbURL = process.env.DATABASEURL || "mongodb+srv://admin:admin@cluster0-5dalc.mongodb.net/MiniCord?retryWrites=true&w=majority";
+config.dbURL = process.env.DATABASEURL || "";
 config.URL = process.env.URL || "http://localhost:5000";
 
 module.exports = config;
